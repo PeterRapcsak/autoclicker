d907d15fb95bb81573b7748cea54a066006a51b1
PeterRapcsak <72269551+PeterRapcsak@users.noreply.github.com>
2023-12-14 19:55:21 +0100

    Van GUI
    aa

--- clicker.py
+++ clicker.py
@@ -2,25 +2,44 @@ import time
 import threading
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
+import customtkinter
 
-TOGGLE_KEY = KeyCode(char="ű")
-clicking = False
+toggle_key = KeyCode.from_char(char="ű")
 mouse = Controller()
+click_thread = None
+clicking = False
 
-def clicker():
-    while True:
+def click_action():
+    global clicking
+    while clicking:
+        mouse.click(Button.left, 1)
+        time.sleep(0.01)
+
+def on_press(key):
+    global click_thread, clicking
+
+    if key == toggle_key:
         if clicking:
-            mouse.click(Button.left, 1)
-        time.sleep(0.0001)
+            clicking = False
+            if click_thread:
+                click_thread.join()  # Wait for the click thread to finish
+        else:
+            clicking = True
+            click_thread = threading.Thread(target=click_action)
+            click_thread.start()
 
-def toggle_event(key):
-    if key == TOGGLE_KEY:
-        global clicking
-        clicking = not clicking
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
 
+app = customtkinter.CTk()
+app.geometry("480x480")
+app.title("Autoclicker")
 
-click_thread = threading.Thread(target=clicker)
-click_thread.start()
+btn = customtkinter.CTkButton(app, text="Save", command=lambda: None)  # No need for command now
+btn.pack(padx=10, pady=10)
 
-with Listener(on_press=toggle_event) as listener:
-    listener.join()
+# Start the keyboard listener in a separate thread
+listener_thread = threading.Thread(target=Listener(on_press=on_press).start)
+listener_thread.start()
+
+app.mainloop()
